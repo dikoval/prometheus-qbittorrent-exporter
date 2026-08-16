f79b388b4c68684671af7f3ccf8dbe72867138fc
Dima Koval <26870220+dikoval@users.noreply.github.com>
2026-08-16 18:20:44 +0300

    lint: lint and format

--- src/cli.rs
+++ src/cli.rs
@@ -1,14 +1,14 @@
-use clap::{Parser};
+use clap::Parser;
 
 #[derive(Parser)]
 pub struct Cli {
-    #[arg(long, default_value="7071")]
+    #[arg(long, default_value = "7071")]
     pub exporter_port: u16,
 
-    #[arg(long, default_value="http://localhost:8080/")]
+    #[arg(long, default_value = "http://localhost:8080/")]
     pub qbittorrent_endpoint: String,
-    #[arg(long, default_value="admin")]
+    #[arg(long, default_value = "admin")]
     pub qbittorrent_username: String,
-    #[arg(long, default_value="adminadmin")]
-    pub qbittorrent_password: String
+    #[arg(long, default_value = "adminadmin")]
+    pub qbittorrent_password: String,
 }
